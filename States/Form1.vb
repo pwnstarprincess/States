d0c417f7d2c34c2415590ea0a6166142602cd86c
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-02-22 23:45:46 -0700

    add subroutine for displaying message output

--- States/Form1.vb
+++ States/Form1.vb
@@ -1,3 +1,66 @@
 ﻿Public Class Form1
 
+    Private Sub ReturnMessage(state As Int16, Category As Int16)
+
+        If (state = 1) Then
+
+            Select Case Category
+                Case "1"
+                    InfoBox.Text = "Golden poppy"
+                Case "2"
+                    InfoBox.Text = "Golden State"
+                Case "3"
+                    InfoBox.Text = "Eureka"
+                Case Else
+                    InfoBox.Text = ""
+            End Select
+        End If
+
+        If (state = 2) Then
+
+            Select Case Category
+                Case "1"
+                    InfoBox.Text = "Peony"
+                Case "2"
+                    InfoBox.Text = "Hoosier State"
+                Case "3"
+                    InfoBox.Text = "Crossroads of America"
+                Case Else
+                    InfoBox.Text = ""
+            End Select
+        End If
+
+        If (state = 3) Then
+
+            Select Case Category
+                Case "1"
+                    InfoBox.Text = "Magnolia"
+                Case "2"
+                    InfoBox.Text = "Magnolia State"
+                Case "3"
+                    InfoBox.Text = "By Valor And Arms"
+                Case Else
+                    InfoBox.Text = ""
+            End Select
+        End If
+
+        If (state = 4) Then
+
+            Select Case Category
+                Case "1"
+                    InfoBox.Text = "Rose"
+                Case "2"
+                    InfoBox.Text = "Empire State"
+                Case "3"
+                    InfoBox.Text = "Ever Upward"
+                Case Else
+                    InfoBox.Text = ""
+            End Select
+        End If
+
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
